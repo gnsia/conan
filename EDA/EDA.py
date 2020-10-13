b64aabc17e31b5ecf939bd67f668032fe668d33f
gnsia <gnsia@naver.com>
2020-10-13 18:10:00 +0900

    use plot & timestamp

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -4,7 +4,7 @@
 # 2. 특정 분(minute)에서 가장 많이 접근한 IP주소와 두번째로 많이 접근한 IP주소가 100번 넘게 차이나면
 # 가장 많이 접근한 IP주소를 이상접근으로 가정한다.
 # 3. UA에 기본 브라우저가 아닌 라이브러리 및 스크립트로 접근
-
+# 4. Referer가 "-"로 되어있는 모든 데이터
 ##############################################
 
 
@@ -14,6 +14,8 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('./dataset/track1.csv')
+df.head(1)
+df.info()
 
 # Timestamp 나누기
 
@@ -38,32 +40,105 @@ df.Bytes.median()
 df.Bytes.max()
 df.Bytes.min()
 
-df.loc[:,'Bytes']
+# Referer가 "-" 로 찍힌 데이터
+rf_mask = df['Referer'].str.contains('"-"')
+df[rf_mask.fillna(False)]['Host'].value_counts()
+
+
+# Referer가 "-"로 찍힌 놈들이 다녀간 Referer
+df[df['Host'] == df[rf_mask.fillna(False)]['Host'].value_counts().index[4]]['Referer'].value_counts()
+
+# Referer가 "-"로 찍힌 놈들이 다녀간 Path
+df[df['Host'] == df[rf_mask.fillna(False)]['Host'].value_counts().index[4]]['Path'].value_counts()
+
+# Referer가 "-"로 찍힌 놈들이 사용한 UA
+df[df['Host'] == df[rf_mask.fillna(False)]['Host'].value_counts().index[4]]['UA'].value_counts()
+
+# Referer가 "-"로 찍힌 놈들이 다녀간 날짜와 시간
+df[df['Host'] == df[rf_mask.fillna(False)]['Host'].value_counts().index[4]]['Date'].value_counts()
+df[df['Host'] == df[rf_mask.fillna(False)]['Host'].value_counts().index[4]]['Hour'].value_counts().sort_index()
+
+
+
+host_counts = pd.DataFrame(df.Host.value_counts())
+host_counts[host_counts['Host'] > 1000].plot()
+
+df[df['Host'] == host_counts[host_counts['Host'] > 3000].index[-1]]['Hour'].value_counts().sort_index()
+
+df[df['Referer'] == '"']['Host'].value_counts()
+
+
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+##########################################날짜별로 나누기#########
+
 
 d = []
 for i in range(len(df['Date'].unique())):
     x = df[df['Date'] == df['Date'].unique()[i]]
     d.append(x)
 
-d[0]['Host'].value_counts()
+# 8/24 분석
+
+len(d[0]['Hour'].value_counts().sort_index())
+d[0]['Hour'].unique()
+d[0][d[0]['Hour'] == d[0]['Hour'].unique()[0]]
+
+
+def subplott():
+    plt.figure(figsize=(20,20))
+    for i in range(1,11):
+        plt.subplot(12,1,i)
+        plt.plot(d[0][d[0]['Hour'] == d[0]['Hour'].unique()[i]]['Min'].value_counts().sort_index())
+        plt.title("f{i}")
+        plt.grid()
+    plt.show()
+
+subplott()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################################
+
+
 d[1]['Host'].value_counts()
+
+d[1]['Hour'].value_counts().sort_index().plot()
+
+####################################################################
+
 d[2]['Host'].value_counts()
+
+d[2]['Hour'].value_counts().sort_index().plot()
+
+################################################################
+
 d[3]['Host'].value_counts()
+d[3]['Hour'].value_counts().sort_index().plot()
+############################################################
+
 d[4]['Host'].value_counts()
+d[4]['Hour'].value_counts().sort_index().plot()
 
-
-
-d24t10 = d24[d24['Hour'] == d24['Hour'].unique()[0]]
-
-d24t10_ano_IP_0 = d24t10[d24t10['Host'] == d24t10['Host'].value_counts().index[0]]
-d24t10_ano_IP_1 = d24t10[d24t10['Host'] == d24t10['Host'].value_counts().index[1]]
-
-X = d24t10['Min']
-Y = d24t10['Sec']
-X_ano_0 = d24t10_ano_IP_0['Min']
-Y_ano_0 = d24t10_ano_IP_0['Sec']
-X_ano_1 = d24t10_ano_IP_1['Min']
-Y_ano_1 = d24t10_ano_IP_1['Sec']
 
 
 plt.figure(figsize=(10,10))
---
+++ EDA/EDA_TimeStamp.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./dataset/track1.csv')
