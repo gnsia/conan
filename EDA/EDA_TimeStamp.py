9f3400e0208666a5a38610ab565e5bb3de106687
gnsia <gnsia@naver.com>
2020-10-14 13:26:41 +0900

    EDA update

--- EDA/EDA_TimeStamp.py
+++ EDA/EDA_TimeStamp.py
@@ -3,4 +3,9 @@ import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('./dataset/track1.csv')
+df = pd.read_csv('./dataset/track1.csv', index_col='Timestamp')
+
+df.head(1)
+df.index = pd.to_datetime(df.index)
+
+df.index[0]
